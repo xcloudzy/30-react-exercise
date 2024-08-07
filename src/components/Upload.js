06b60161e622c57e5a86e2e087b436f5260390f8
Amit Kumar <amit4.jammu@gmail.com>
2024-08-07 12:19:15 +0530

    added

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -3,14 +3,24 @@ import { useState } from "react";
 const Upload = () => {
   const [file, setFile] = useState(null);
   return (
-    <div style={{ backgroundColor: "#242424", color: "white" }}>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={(e) => setFile(e.target.files[0])}
-      />
-      {file && <img src={URL.createObjectURL(file)} alt="Loading" />}
-    </div>
+    <>
+      <div style={{ backgroundColor: "#242424", color: "white" }}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <div>
+          {file && (
+            <img
+              class="img-fluid"
+              src={URL.createObjectURL(file)}
+              alt="Loading"
+            />
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
